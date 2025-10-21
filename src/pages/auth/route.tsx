ee6c33f2d682f1bf2f98624e026236b991cb94e4
natasharockbell-pixel <natasharockbell@gmail.com>
2025-10-21 22:07:29 +0200

    Delete src/pages/auth directory

--- src/pages/auth/route.tsx
+++
@@ -1,9 +0,0 @@
-import { RouteDefinition } from 'solid-app-router'
-import AuthPage from './auth'
-
-const authRoute: RouteDefinition = {
-  path: '/auth',
-  component: AuthPage,
-}
-
-export default authRoute
