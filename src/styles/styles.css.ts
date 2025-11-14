945de3c98be24e0eb2485d4037927db326e73cea
natasharockbell-pixel <natasharockbell@gmail.com>
2025-11-14 15:30:57 +0200

    Update styles.css.ts

--- src/styles/styles.css.ts
+++ src/styles/styles.css.ts
@@ -1,5 +1,8 @@
 import { vars } from './vars.css'
 import { sprinkles } from './sprinkles.css'
-import * as sharedStyles from './shared.css'
+import * as _shared from './shared.css'
 
-export { vars, sprinkles, sharedStyles }
+// Explicitly assign to constants (avoids any re-export shorthand that could create getter proxies)
+export const sharedStyles = _shared
+export const exportedVars = vars
+export const exportedSprinkles = sprinkles
